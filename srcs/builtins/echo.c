849eafc417559418aadd0c4910a280d3cc777f21
Roch Blondiaux <contact@roch-blondiaux.com>
2022-02-08 00:15:59 +0100

    Echo fix

--- srcs/builtins/echo.c
+++ srcs/builtins/echo.c
@@ -16,24 +16,20 @@ void	builtin_echo(char **args)
 {
 	int		mode;
 	size_t	i;
-	size_t	l;
 
-	i = -1;
+	i = 0;
 	mode = 0;
-	l = array_length(args);
-	if (l > 0 && ft_strcmp(args[0], "-n"))
-	{
+	while (ft_strcmp_sensitive(args[i++], "-n"))
 		mode = 1;
-		i += 1;
-	}
+	i -= 2;
 	while (args[++i])
 	{
-		if (i == l - 1)
+		if (i == array_length(args) - 1)
 			printf("%s", args[i]);
 		else
 			printf("%s ", args[i]);
 	}
-	if (l > 0)
+	if (array_length(args) > 0)
 	{
 		if (mode)
 			printf("%s%s%%%s", BOLD, "\033[3;107;30m", RESET);
