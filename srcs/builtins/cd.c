d5b4e679739b330330ca86f5990731289c4f878e
Draxuxe <louis.filloux53@gmail.com>
2022-02-08 14:43:50 +0100

    Cd - fixed and normed

--- srcs/builtins/cd.c
+++ srcs/builtins/cd.c
@@ -53,29 +53,46 @@ static int	check_existing_cd(t_app *app, char *dest, char **args, size_t i)
 	return (0);
 }
 
+static int	special_cd_path(t_app *app, char **args, char *current)
+{
+	if (ft_strcmp(args[0], "/"))
+	{
+		set_path(app, ft_strdup("/"));
+		return (0);
+	}
+	else if (ft_strcmp(args[0], "-"))
+	{
+		set_path(app, app->last_path);
+		app->last_path = ft_strdup(current);
+		free(current);
+		return (0);
+	}
+	return (1);
+}
+
 void	builtin_cd(t_app *app, char **args)
 {
 	char	**dest;
+	char	*current;
 	size_t	i;
 
+	current = working_directory();
 	if (array_length(args) == 0)
 		set_path(app, home_directory());
 	else if (array_length(args) == 1)
 	{
-		if (ft_strcmp(args[0], "/"))
-			set_path(app, ft_strdup("/"));
+		if (special_cd_path(app, args, current) == 0)
+			return ;
 		i = -1;
 		dest = ft_split(args[0], '/');
 		while (dest[++i])
-		{
 			if (check_existing_cd(app, dest[i], args, i) != 0)
-			{
-				free_array(dest);
-				return ;
-			}
-		}
+				break ;
 		free_array(dest);
 	}
 	else
 		str_error(app, TOO_MANY_ARGUMENTS);
+	free(app->last_path);
+	app->last_path = ft_strdup(current);
+	free(current);
 }
