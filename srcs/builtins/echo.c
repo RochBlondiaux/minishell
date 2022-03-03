a5fb6538db802cdff2f151308de3750bc4d1d748
Draxuxe <louis.filloux53@gmail.com>
2022-03-03 12:36:44 +0100

    echo normed

--- srcs/builtins/echo.c
+++ srcs/builtins/echo.c
@@ -25,6 +25,20 @@ static int	is_del_nl(char *param)
 	return (0);
 }
 
+static void	print_arg(t_app *app, t_command *cmd, size_t i)
+{
+	if (cmd->args[i][0] == '$' && cmd->args[i][1] == '?'
+		&& !cmd->args[i][2])
+	{
+		free(cmd->args[i]);
+		cmd->args[i] = ft_itoa(app->last_exit);
+	}
+	if (i == array_length(cmd->args) - 1)
+		printf("%s", cmd->args[i]);
+	else
+		printf("%s ", cmd->args[i]);
+}
+
 void	builtin_echo(t_app *app, t_command *cmd)
 {
 	int		mode;
@@ -42,18 +56,7 @@ void	builtin_echo(t_app *app, t_command *cmd)
 		mode = 1;
 	i -= 2;
 	while (cmd->args[++i])
-	{
-		if (cmd->args[i][0] == '$' && cmd->args[i][1] == '?'
-			&& !cmd->args[i][2])
-		{
-			free(cmd->args[i]);
-			cmd->args[i] = ft_itoa(app->last_exit);
-		}
-		if (i == array_length(cmd->args) - 1)
-			printf("%s", cmd->args[i]);
-		else
-			printf("%s ", cmd->args[i]);
-	}
+		print_arg(app, cmd, i);
 	if (array_length(cmd->args) > 0)
 	{
 		if (mode)
